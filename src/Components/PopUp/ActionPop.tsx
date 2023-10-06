7f205f3c9138067fc2f72439ace9806e229c6dac
Samar Raj <samar.suvaidyam@gmail.com>
2023-10-06 17:17:15 +0530

    add

--- src/Components/PopUp/ActionPop.tsx
+++ src/Components/PopUp/ActionPop.tsx
@@ -10,48 +10,45 @@ interface Action {
 }
 interface ActionPopProps {
     action: Action[]
-    icon: 'FiMoreVertical';
+    icon: 'FiMoreVertical' | 'none';
 }
 
 
 const ActionPop: FC<ActionPopProps> = ({ action, icon }) => {
     return (
-        <>
-            <Menu as="div" className="relative inline-block text-left">
-                <div>
-                    <Menu.Button className={'text-2xl'}>
-                        {icon === 'FiMoreVertical' && <FiMoreVertical />}
-                    </Menu.Button>
-                </div>
-                <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="py-1">
-                            {action?.length > 0 && action?.map(e => (
-                                <React.Fragment key={e?.id}>
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <p onClick={e.click} className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'px-4 h-10 font-medium cursor-pointer flex items-center')} >
-                                                {e?.name}
-                                            </p>
-                                        )}
-                                    </Menu.Item>
-                                </React.Fragment>
-                            ))}
-                        </div>
-                    </Menu.Items>
-                </Transition>
-            </Menu>
-        </>
+        <Menu as="div" className="relative inline-block text-left w-full h-full">
+            <Menu.Button className={'text-2xl w-full h-full'}>
+                {icon === 'FiMoreVertical' && <FiMoreVertical />}
+                {icon === 'none' && <span className='w-full h-full'></span>}
+            </Menu.Button>
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                        {action?.length > 0 && action?.map(e => (
+                            <React.Fragment key={e?.id}>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <p onClick={e.click} className={classNames(
+                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'px-4 h-10 font-medium cursor-pointer flex items-center')} >
+                                            {e?.name}
+                                        </p>
+                                    )}
+                                </Menu.Item>
+                            </React.Fragment>
+                        ))}
+                    </div>
+                </Menu.Items>
+            </Transition>
+        </Menu>
     )
 }
 
--- src/Components/PopUp/ProfilePop.tsx
+++ src/Components/PopUp/ProfilePop.tsx
@@ -9,6 +9,8 @@ import http from '../../Services/http/http';
 import { toast } from 'react-toastify';
 import * as Yup from 'yup'
 import GenderRadioGroup from '../FormControl/GenderRadioGroup';
+import { AnimatePresence, motion } from 'framer-motion'
+import ActionPop from './ActionPop';
 
 interface ProfilePopProps {
     open: boolean
@@ -37,7 +39,7 @@ const ProfilePop: FC<ProfilePopProps> = ({ open, setOpen }) => {
     }
     useEffect(() => {
         fetchUsernames()
-        if(open === true){
+        if (open === true) {
             setDisableFields(true)
         }
         // eslint-disable-next-line
@@ -57,41 +59,68 @@ const ProfilePop: FC<ProfilePopProps> = ({ open, setOpen }) => {
     });
 
     async function checkUsernameUniqueness(username: string) {
-        return !usernames?.find((user: IUsers) => user.username === username) ? true : false;
+        return !usernames?.find((user: IUsers) => user.username !== userDetails.username && user.username === username) ? true : false;
     }
+    const ViewProfileImage = () => {
+        window.alert('View Profile Image')
+    }
+    const EditProfileImage = () => {
+        window.alert('Edit Profile Image')
+    }
+    const actions = [
+        { id: 1, name: 'View Profile Image', click: ViewProfileImage },
+        { id: 2, name: 'Edit Profile Image', click: EditProfileImage }
+    ]
     const handleSubmit = (values: any) => {
         console.log(values)
     }
     return (
-        <div onClick={() => setOpen(false)} className={`${open ? 'absolute z-50 left-0 top-0 w-screen flex items-center justify-center h-screen bg-black bg-opacity-70' : 'hidden'} `}>
-            <div onClick={(event) => handleChildClickPrevent(event)} className="sm:w-[450px] flex flex-col gap-3 items-center sm:mx-0 mx-6 w-full sm:min-w-[450px] h-auto p-2 bg-white">
-                <div className="w-20 h-20 min-h-20 min-w-20 rounded-full border">
-                    <img src={userDetails.profileImage === null ? (userDetails.gender === 'male' ? maleavatar : userDetails.gender === 'female' ? femaleavatar : otheravatar) : `${userDetails.profileImage.url}`} alt={'profile'} className='w-full h-full rounded-full cursor-pointer' />
-                </div>
-                <div className="w-full">
-                    <Formik
-                        initialValues={{
-                            name: userDetails.name,
-                            username: userDetails.username,
-                            gender: userDetails.gender
-                        }}
-                        validationSchema={validationSchema}
-                        onSubmit={handleSubmit}
+        <AnimatePresence>
+            {open && (
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    onClick={() => setOpen(false)}
+                    className={`absolute z-50 left-0 top-0 w-screen flex items-center justify-center h-screen bg-black bg-opacity-70`}
+                >
+                    <motion.div
+                        initial={{ opacity: 0, y: -70 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -70 }}
+                        onClick={(event) => handleChildClickPrevent(event)}
+                        className="sm:w-[450px] rounded-lg shadow-2xl shadow-blue-100 flex flex-col gap-3 items-center sm:mx-0 mx-6 w-full sm:min-w-[450px] h-auto px-4 py-8 bg-white"
                     >
-                        <Form className='flex flex-col gap-5 p-2'>
-                            <InputText name='name' label='Name' id='name' required={false} disabled={disableFields} />
-                            <InputText name='username' label='Username' id='username' required={false} disabled={disableFields} />
-                            <Field component={() => <GenderRadioGroup disabled={disableFields} />} name='gender' disabled={true} />
-                            {disableFields ?
-                                <span onClick={() => setDisableFields(false)} className='font-medium flex justify-center cursor-pointer text-white bg-blue-600 disabled:bg-blue-300 w-full py-1.5 rounded-md hover:bg-blue-700'>Edit Profile</span>
-                                :
-                                <button type='submit' className='font-medium text-white bg-blue-600 disabled:bg-blue-300 w-full py-1.5 rounded-md hover:bg-blue-700'>Save</button>
-                            }
-                        </Form>
-                    </Formik>
-                </div>
-            </div>
-        </div>
+                        <div className="w-24 h-24 min-h-24 min-w-24 rounded-full border relative">
+                            <img src={userDetails.profileImage === null ? (userDetails.gender === 'male' ? maleavatar : userDetails.gender === 'female' ? femaleavatar : otheravatar) : `${userDetails.profileImage.url}`} alt={'profile'} className='w-full h-full rounded-full cursor-pointer' />
+                            <span className='absolute top-0 left-0 w-full h-full'><ActionPop action={actions} icon='none' /></span>
+                        </div>
+                        <div className="w-full">
+                            <Formik
+                                initialValues={{
+                                    name: userDetails.name,
+                                    username: userDetails.username,
+                                    gender: userDetails.gender
+                                }}
+                                validationSchema={validationSchema}
+                                onSubmit={handleSubmit}
+                            >
+                                <Form className='flex flex-col gap-5 p-2'>
+                                    <InputText name='name' label='Name' id='name' required={false} disabled={disableFields} />
+                                    <InputText name='username' label='Username' id='username' required={false} disabled={disableFields} />
+                                    <Field component={() => <GenderRadioGroup disabled={disableFields} />} name='gender' disabled={true} />
+                                    {disableFields ?
+                                        <span onClick={() => setDisableFields(false)} className='font-medium flex justify-center cursor-pointer text-white bg-blue-600 disabled:bg-blue-300 w-full py-1.5 rounded-md hover:bg-blue-700'>Edit Profile</span>
+                                        :
+                                        <button type='submit' className='font-medium text-white bg-blue-600 disabled:bg-blue-300 w-full py-1.5 rounded-md hover:bg-blue-700'>Save</button>
+                                    }
+                                </Form>
+                            </Formik>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence >
     );
 }
 
